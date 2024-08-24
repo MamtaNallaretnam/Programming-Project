10b7a45e048d3eb6e7a684b5e0e19bb4d69f9b5a
Mamta <129370981+MamtaNallaretnam@users.noreply.github.com>
2024-08-24 19:50:05 +0530

    Delete RPAL Parser directory

--- RPAL Parser/parser/StandardizeException.java
+++
@@ -1,9 +0,0 @@
-package parser;
-public class StandardizeException extends RuntimeException {
-  private static final long serialVersionUID = 1L;
-
-  public StandardizeException(String message) {
-    super(message);
-  }
-
-}
